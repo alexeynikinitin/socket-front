86a04b611bd87c537d143a17ca1e5acfe990da3e
alexeynikinitin <alexeynikinitin@yandex.by>
2022-05-12 21:19:41 +0300

    connect socket

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,83 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import {io} from "socket.io-client";
+
+const socket = io("http://localhost:8080");
 
 function App() {
    const [myFormClassName, setMyFormClassName] = useState("myForm__close")
    const [myButtonClassName, setButtonClassName] = useState("myForm__open")
+   const [message, setMessage] = useState("")
+   const [chat, setChat] = useState<PayloadType[]>([])
+
+   const sendChat = () => {
+      // e.preventDefault();
+      socket.emit("client-message-sent", { message })
+      setMessage("")
+   }
+
    function openForm() {
       setMyFormClassName("myForm__open")
       setButtonClassName("myButton__close")
    }
-
    function closeForm() {
       setMyFormClassName("myForm__close")
       setButtonClassName("myButton__open")
    }
 
+   useEffect(() => {
+      socket.on("client-message-sent", (payload: PayloadType) => {
+         setChat([...chat, payload])
+      })
+   })
+
+   useEffect(() => {
+      socket.on('init-messages-loaded', (payload: PayloadType[]) => {
+         setChat(payload)
+      })
+   }, [])
+
    return (
       <div className="App">
-         <div className="container">
-            <img src="https://www.w3schools.com/w3images/bandmember.jpg" alt="Avatar"/>
-               <p>Hello. How are you today?</p>
-               <span className="time-right">11:00</span>
-         </div>
 
-         <div className="container darker">
-            <img src="https://www.w3schools.com/w3images/avatar_g2.jpg" alt="Avatar" className="right"/>
-               <p>Hey! I'm fine. Thanks for asking!</p>
-               <span className="time-left">11:01</span>
-         </div>
+         {chat.map((payload, index) => {
+            return (
+               <div className="container" key={`${payload.message}-${index}`}>
+                  <img src="https://www.w3schools.com/w3images/bandmember.jpg"
+                       alt="Avatar"/>
+                  <p>{payload.message}</p>
+                  <span className="time-right">11:00</span>
+               </div>
+            )
+         })}
 
-         <button className="open-button" id={myButtonClassName} onClick={openForm}>Chat</button>
+         <button className="open-button" id={myButtonClassName} onClick={openForm}>Chat
+         </button>
 
          <div className="chat-popup" id={myFormClassName}>
-            <form action="#" className="form-container">
+            <div className="form-container">
                <h1>Chat</h1>
 
                <label htmlFor="msg"><b>Message</b></label>
-               <textarea placeholder="Type message.." name="msg" required/>
-
-               <button type="submit" className="btn">Send</button>
+               <textarea
+                  placeholder="Type message.."
+                  name="msg"
+                  required
+                  value={message}
+                  onChange={(e) => setMessage(e.currentTarget.value)}
+               />
+               <button className="btn" onClick={sendChat}>Send
+               </button>
                <button type="button" className="btn cancel" onClick={closeForm}>Close
                </button>
-            </form>
+            </div>
          </div>
       </div>
-);
+   );
 }
 
 export default App;
+
+type PayloadType = {
+   message: string;
+}
