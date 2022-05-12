e767d7e47a0fe1a2aa1b2e34976c1efd60181cc0
alexeynikinitin <alexeynikinitin@yandex.by>
2022-05-12 21:46:49 +0300

    added "init-messages-loaded" and "new-message-sent" events

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,42 @@
-import React, {useEffect, useState} from 'react';
 import './App.css';
 import {io} from "socket.io-client";
+import React, {useEffect, useState} from 'react';
 
 const socket = io("http://localhost:8080");
 
 function App() {
-   const [myFormClassName, setMyFormClassName] = useState("myForm__close")
-   const [myButtonClassName, setButtonClassName] = useState("myForm__open")
-   const [message, setMessage] = useState("")
-   const [chat, setChat] = useState<PayloadType[]>([])
+   const [myFormClassName, setMyFormClassName] = useState("myForm__close");
+   const [myButtonClassName, setButtonClassName] = useState("myForm__open");
+
+   const [message, setMessage] = useState("");
+   const [chat, setChat] = useState<PayloadType[]>([]);
 
    const sendChat = () => {
       // e.preventDefault();
-      socket.emit("client-message-sent", { message })
-      setMessage("")
-   }
+      socket.emit("client-message-sent", { message });
+      setMessage("");
+   };
 
-   function openForm() {
-      setMyFormClassName("myForm__open")
-      setButtonClassName("myButton__close")
-   }
-   function closeForm() {
-      setMyFormClassName("myForm__close")
-      setButtonClassName("myButton__open")
-   }
+   const openForm = () => {
+      setMyFormClassName("myForm__open");
+      setButtonClassName("myButton__close");
+   };
+   const closeForm = () => {
+      setMyFormClassName("myForm__close");
+      setButtonClassName("myButton__open");
+   };
 
    useEffect(() => {
-      socket.on("client-message-sent", (payload: PayloadType) => {
-         setChat([...chat, payload])
-      })
-   })
+      socket.on("new-message-sent", (payload: PayloadType) => {
+         setChat([...chat, payload]);
+      });
+   });
 
    useEffect(() => {
       socket.on('init-messages-loaded', (payload: PayloadType[]) => {
-         setChat(payload)
-      })
-   }, [])
+         setChat(payload);
+      });
+   }, []);
 
    return (
       <div className="App">
@@ -51,7 +52,12 @@ function App() {
             )
          })}
 
-         <button className="open-button" id={myButtonClassName} onClick={openForm}>Chat
+         <button
+            onClick={openForm}
+            id={myButtonClassName}
+            className="open-button"
+         >
+            Chat
          </button>
 
          <div className="chat-popup" id={myFormClassName}>
@@ -60,15 +66,19 @@ function App() {
 
                <label htmlFor="msg"><b>Message</b></label>
                <textarea
-                  placeholder="Type message.."
-                  name="msg"
                   required
+                  name="msg"
                   value={message}
+                  placeholder="Type message.."
                   onChange={(e) => setMessage(e.currentTarget.value)}
                />
-               <button className="btn" onClick={sendChat}>Send
-               </button>
-               <button type="button" className="btn cancel" onClick={closeForm}>Close
+               <button className="btn" onClick={sendChat}>Send</button>
+               <button
+                  type="button"
+                  onClick={closeForm}
+                  className="btn cancel"
+               >
+                  Close
                </button>
             </div>
          </div>
